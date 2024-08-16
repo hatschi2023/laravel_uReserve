964cb5817f98e209ba6ec08b4192923cf43a23d5
hatschi2023 <145645279+hatschi2023@users.noreply.github.com>
2024-08-16 23:37:56 +0900

    flatpickr

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -30,7 +30,7 @@ class CreateNewUser implements CreatesNewUsers
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
-            'role' => 9,
+            'role' => 9, // ユーザー登録する際はuserとして追加
         ]);
     }
 }
