e6ffdf8665a48cfd52249e33f6986387934c0c47
hatschi2023 <145645279+hatschi2023@users.noreply.github.com>
2024-08-23 16:16:17 +0900

    event reserve

--- resources/views/layouts/calendar.blade.php
+++ resources/views/layouts/calendar.blade.php
@@ -39,7 +39,6 @@
             </main>
         </div>
 
-        <!-- @livewire('calendar') -->
         @livewireScripts
     </body>
 </html>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,3 +1,7 @@
+@if (Auth::check())
+
+@endif
+
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -13,7 +17,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        イベントカレンダー
                     </x-nav-link>
                     @can('manager-higher')
                     <x-nav-link href="{{ route('events.index') }}" :active="request()->routeIs('events.index')">
@@ -23,19 +27,22 @@
                 </div>
             </div>
 
+
+
+
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <!-- Teams Dropdown -->
 
                 {{-- 仮：ログインしていないとエラーが出るため --}}
-                @if (!Auth::check())
+                {{-- @if (!Auth::check())
                     <span class="inline-flex rounded-md">
                         <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50">
                             Guest
                         </button>
                     </span>
-                @endif
+                @endif --}}
 
-                
+
                 @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
                     <div class="ml-3 relative">
                         <x-dropdown align="right" width="60">
@@ -90,8 +97,12 @@
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
 
+
+
                                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                                     <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+
+
                                         <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                                     </button>
                                 @else
@@ -105,7 +116,6 @@
                                         </button>
                                     </span>
                                 @endif
-
                         </x-slot>
 
                         <x-slot name="content">
@@ -140,6 +150,8 @@
                 </div>
             </div>
 
+
+
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
@@ -156,7 +168,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                イベントカレンダー
             </x-responsive-nav-link>
             @can('manager-higher')
             <x-responsive-nav-link href="{{ route('events.index') }}" :active="request()->routeIs('events.index')">
@@ -235,4 +247,6 @@
             </div>
         </div>
     </div>
+
 </nav>
+
